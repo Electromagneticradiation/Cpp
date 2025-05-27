885f3c085f7e23856687d59a3e0c35fbfb171be6
Snow <githubwithsnow@gmail.com>
2025-05-27 23:44:42 +0530

    loop 1 : special numbers

---
+++ Basic/loop1/main.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+bool is_special(int spec, int inp) {
+    while (spec>0) {
+        // do with TRY CATCH later
+        if (spec % 10 == 0) {
+            return false;
+        } else if (inp % (spec%10) == 0) {
+            spec /= 10;
+        } else {
+            return false;
+        }
+    }
+    return true;
+}
+
+// take an int input and print all special num from 111-999
+
+int main() {
+    int n;
+    std::cout << "What's the number?  ";
+    std::cin >> n;
+    for (int i=111; i<1000; i++) {
+        if (is_special(i,n)) {
+            std::cout << i << " ";
+        }
+    }
+}
